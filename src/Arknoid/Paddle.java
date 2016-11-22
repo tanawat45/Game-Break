c94efddf649cd418908981b9d413becc6a99c560
apboomap277 <tanin.wan7275@gmail.com>
2016-11-22 14:05:35 +0700

    ver2.3

--- src/Arknoid/Paddle.java
+++ src/Arknoid/Paddle.java
@@ -2,7 +2,7 @@ package Arknoid;
 
 import javax.swing.*;
 import java.awt.event.KeyEvent;
-//
+
 public class Paddle extends Sprite implements Commons{
     private int dx;
     private int type = 0;
@@ -10,7 +10,7 @@ public class Paddle extends Sprite implements Commons{
     private int moveState ;
     private int moveSpeed ;
     ImageIcon ii;
-
+//
     public Paddle() {
         ii = new ImageIcon("paddle1.png");
         image = ii.getImage();
